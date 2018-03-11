907802cec1293a350fe9e708dfb6f722555e3b88
Trisha Garrett <trisha.garrett@gmail.com>
2018-03-11 22:26:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your code in this file!
 const currentUser = "Grace Hopper";
 
-const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const welcomeMessage = "Welcome to Flatbook, ${currentUser}!";
 
-let excitedWelcomeMessage;
-excitedWelcomeMessage = "Welcome to Flatbook!"
+
